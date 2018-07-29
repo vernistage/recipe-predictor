92e554326e4f3b75e999a42d4270362dcbbb592a
Sarah Colby <vernistage@users.noreply.github.com>
2018-07-29 13:59:25 -0500

    Add recipe info (#6)
    * add more requirements
    
    * make recipes equal height
    
    * send proper dataset
    
    * more styling
    
    * we done

--- ingredients/views.py
+++ ingredients/views.py
@@ -10,9 +10,17 @@ def ingredient_input(request):
         if form.is_valid():
             ingredient = form.cleaned_data['ingredient']
             context['form'] = IngredientForm()
-            context['recipes'] = form.send_ingredient("./word2vec_model", ingredient)
+            recipes = form.send_ingredient("./word2vec_model", ingredient)
+            context['recipes'] = recipes[:3]
+            context['recommended_recipes'] = recipes[3:9]
+            context['ingredient'] = ingredient
             return render(request, 'ingredients/ingredient_input.html', context)
     else:
         form = IngredientForm()
         context['form'] = form
         return render(request, 'ingredients/ingredient_input.html', context)
+
+# Index(['calories', 'categories', 'date', 'desc', 'directions', 'fat',
+#        'ingredients', 'protein', 'rating', 'sodium', 'title',
+#        'gensim_directions', 'gensim_directions_doc', 'vec', 'diff'],
+#       dtype='object')
