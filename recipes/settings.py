9a722ee5c0e29df8f27c9d8b2b09321c661d09a4
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2018-08-04 14:51:06 -0500

    remove Welcome module

--- recipes/settings.py
+++ recipes/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'recipes',
-    'welcome',
     'ingredients',
     'django.contrib.admin',
     'django.contrib.auth',
